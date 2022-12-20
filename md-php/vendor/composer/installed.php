bd6e00326b9cbf6830420a4ea327a0f62e27b1d2
Germán Huaytalla <germanhuaytalla22@gmail.com>
2022-12-19 21:13:39 -0500

    Add dependencias activemq stomp en módulo procesamiento órdenes

---
+++ md-php/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e92ebfc297e48c64b8368dd2691082cad181a953',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e92ebfc297e48c64b8368dd2691082cad181a953',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stomp-php/stomp-php' => array(
+            'pretty_version' => '5.0.0',
+            'version' => '5.0.0.0',
+            'reference' => '50f6e6e9aa1ba4696faa40cd1cbe180a96679f98',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stomp-php/stomp-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
