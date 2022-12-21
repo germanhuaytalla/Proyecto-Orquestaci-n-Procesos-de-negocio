ac6801aaa6833c69211dfdf88641e896e57d0045
ChotyGod <72470226+Choty00@users.noreply.github.com>
2022-12-21 11:37:40 -0500

    Formato de mensaje incluido
    Se hace la verificación y reserva con el último formato de mensaje.

--- md-python/consumidor.py
+++ md-python/consumidor.py
@@ -6,82 +6,80 @@ from productor import enviarMensaje
 from settings import settings
 
 class ListenerArticulos(stomp.ConnectionListener):
+    def __init__(self, conn):
+        self.conn = conn
+
     def on_error(self, message):
-        print('error "%s"' % message.body)
+        print('error 1 "%s"' % message.body)
+
     def on_message(self, message):
         print('lista de articulos recibida con exito "%s' % message.body)
-        lista_articulos = message.body
-        validarArticulos(lista_articulos)
-
+        print(message.body)
+        validarArticulos(message.body)
+        conn.disconnect()
+    
 class ListenerConfirmacion(stomp.ConnectionListener): 
-    def __init__(self):
-        global confirmacion
-        confirmacion = False
+    def __init__(self, conn):
+        print("listener confirmacion")
+        self.conn = conn
         
     def on_error(self, message):
-        print('error "%s"' % message.body)
+        print('error 2"%s"' % message.body)
 
     def on_message(self, message):
         global confirmacion
-        confirmacion = True
-        print('confirmacion recibida con exito "%s' % message.body)
+        confirmacion = int(message.body)
+        print('confirmacion recibida con exito "%s"' % message.body)
+        conn.disconnect()
 
-    def on_disconnected(self):
-        print('disconnected')
-        
-
-def recibirMensaje():
-    hosts = [(settings.MYSQL_HOST, settings.ACTIVEMQ_PORT)]
-    conn = stomp.Connection(host_and_ports=hosts)
-    conn.set_listener('', ListenerArticulos())
-    conn.connect(wait=True, headers = {'client-id': 'clientname'} )
-    conn.subscribe(destination='ordenes/lista_articulos', id=123, ack='auto', headers = {'subscription-type': 'MULTICAST','durable-subscription-name':'articulos'})
-    time.sleep(int(settings.TIEMPO_ESPERA) + 5)
-    # print("DESCONECTANDO...")
-    
-def validarArticulos(lista):
-    lista_articulos = json.loads(lista)
+def validarArticulos(mensaje):
+    datos = json.loads(mensaje)
+    items = datos['mensaje']['items']
     print("Validando lista de artículos")
-    cnn = mysql.connector.connect(host="localhost", user="root", passwd="", database="fisi_tiendasutiles")
+    cnn = mysql.connector.connect(host=settings.MYSQL_HOST, user=settings.MYSQL_USER, passwd=settings.MYSQL_PASSWORD, database=settings.MYSQL_DB)
     cur = cnn.cursor()
     i = 0
-    print(lista_articulos)
-    for codigo in lista_articulos:
-        print(codigo)
-        cur.execute("SELECT cantidad FROM articulos where codigo = {}".format(codigo))
+    for item in items:
+        print(item['codigo'])
+        cur.execute("SELECT cantidad FROM articulos where codigo = {}".format(item['codigo']))
         stock = cur.fetchall()
-        stock = stock[0]
         if cur.rowcount == 0:
-            mensaje = 'El articulo {} no existe'.format(codigo)
+            mensaje = 'El articulo {} no existe'.format(item['codigo'])
             print(mensaje)
-            enviarMensaje('ordenes/error', mensaje)
+            # se envía mensaje de error al módulo de órdenes
+            enviarMensaje(settings.TOPIC_TO_1, mensaje)
             break
         else:
-            if stock[0] >= int(lista_articulos[codigo]):
+            stock = int(stock[0][0])
+            if stock >= int(item['cantidad']):
                 i = i + 1
-                if i == len(lista_articulos):
+                if i == len(items):
                     print("RESERVANDO ARTICULOS...")
-                    reservar(lista_articulos)  
+                    reservar(items)  
             else:
-                mensaje = 'No hay stock suficiente del articulo {}. Solo hay {} unidades'.format(codigo, stock[0])
+                mensaje = 'No hay stock suficiente del articulo {}. Solo hay {} unidades'.format(item['codigo'], stock)
                 print(mensaje)
-                enviarMensaje('ordenes/error', mensaje)
+                enviarMensaje(settings.TOPIC_TO_1, mensaje)
                 break
 
-def reservar(lista_articulos):
-    cnn = mysql.connector.connect(host="localhost", user="root", passwd="", database="fisi_tiendasutiles")
+def reservar(items):
+    cnn = mysql.connector.connect(host=settings.MYSQL_HOST, user=settings.MYSQL_USER, passwd=settings.MYSQL_PASSWORD, database=settings.MYSQL_DB)
     cur = cnn.cursor()
-    for codigo in lista_articulos:
-        cur.execute("UPDATE articulos SET cantidad = cantidad - {}  WHERE (codigo = {})".format(lista_articulos[codigo], codigo))
+    for item in items:
+        cur.execute("UPDATE articulos SET cantidad = cantidad - {}  WHERE (codigo = {})".format(item['codigo'], item['cantidad']))
         cnn.commit()
     
-    hosts = [(settings.MYSQL_HOST, settings.ACTIVEMQ_PORT)]
-    conn = stomp.Connection(host_and_ports=hosts)
-    conn.set_listener('', ListenerConfirmacion())
-    conn.connect(wait=True, headers = {'client-id': 'clientname'} )
-    conn.subscribe(destination='ordenes/confirmacion', id=987, ack='auto',headers = {'subscription-type': 'MULTICAST','durable-subscription-name':'confirmaciones'})
-
+    # Solicitando confirmación del cliente en el módulo de procesamiento de órdenes
     print("ESPERANDO CONFIRMACION")
+    mensaje = '{"estado": 1, "mensaje": "solicitando confirmacion"}'
+    enviarMensaje(settings.TOPIC_TO_1, mensaje)
+
+    # Escuchando mensaje de confirmación
+    hosts = [(settings.ACTIVEMQ_HOST, settings.ACTIVEMQ_PORT)]
+    conn = stomp.Connection(host_and_ports=hosts)
+    conn.set_listener('', ListenerConfirmacion(conn))
+    conn.connect(wait=True, headers = {'client-id': 'fisi_utiles'} )
+    conn.subscribe(destination=settings.TOPIC_FROM, id=987, ack='auto',headers = {'subscription-type': 'ANYCAST','durable-subscription-name':'confirmacion'})
 
     for x in range(int(settings.TIEMPO_ESPERA), 0, -1):
         seconds = x % 60
@@ -90,19 +88,28 @@ def reservar(lista_articulos):
         print(f"{hours:02}:{minutes:02}:{seconds:02}")
         time.sleep(1)
         print(confirmacion)
-        if confirmacion == True:
+        if confirmacion == 1:
             print("CONFIRMACION DE LA COMPRA")
             mensaje = 'Enviando mensaje al módulo de Facturación'
             print(mensaje)
-            enviarMensaje('facturacion', mensaje)
+            enviarMensaje(settings.TOPIC_TO_2, mensaje)
             break
-    
-    if confirmacion == False:
+
+    if confirmacion == 0:
         print("NO SE REALIZO LA CONFIRMACION DE LA COMPRA")
-        for codigo in lista_articulos:
-            cur.execute("UPDATE articulos SET cantidad = cantidad + {}  WHERE (codigo = {})".format(lista_articulos[codigo], codigo))
+        for item in items:
+            cur.execute("UPDATE articulos SET cantidad = cantidad + {}  WHERE (codigo = {})".format(item['codigo'], item['cantidad']))
             cnn.commit()
 
-if __name__ == '__main__':
-    confirmacion = False
-    recibirMensaje()
+    conn.disconnect()
+
+
+confirmacion = 0        
+hosts = [(settings.ACTIVEMQ_HOST, settings.ACTIVEMQ_PORT)]
+conn = stomp.Connection(host_and_ports=hosts)
+conn.set_listener('', ListenerArticulos(conn))
+conn.connect(wait=True, headers = {'client-id': 'fisi_utiles'} )
+conn.subscribe(destination=settings.TOPIC_FROM, id=123, ack='auto', headers = {'subscription-type': 'ANYCAST','durable-subscription-name':'inventario'})
+print("Esperando mensajes...")
+while 1:
+    time.sleep(10)
