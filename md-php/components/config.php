c0523ee32621a457b19c0944618232de18f1a472
Germán Huaytalla <germanhuaytalla22@gmail.com>
2022-12-21 15:23:53 -0500

    Avance

--- md-php/components/config.php
+++ md-php/components/config.php
@@ -3,6 +3,17 @@
   define("dbname","fisi_tiendasutiles");
   define("user","postgres");
   define("password","admin");
+
+  define("POSTGRESQL_HOST","localhost");
+  define("POSTGRESQL_DB","fisi_tiendasutiles");
+  define("POSTGRESQL_USER","postgres");
+  define("POSTGRESQL_PASSWORD","admin");
+  define("ACTIVEMQ_PORT","61616");
+  define("TOPIC_FROM","administraciondeinventarioyreserva");
+  define("TOPIC_TO","administraciondeinventarioyreserva");
+  define("URL","tcp://localhost:61616");
+
+  
   
   // echo " user=".constant("user")." password=".constant("password")."host=".constant("host")." dbname=".constant("dbname")."";
   
